35282a5af285893a3d360bf98d6ff19c0e3de8ae
Zver <Zver@zver.zver>
2024-06-19 10:37:08 +0300

    aspose

--- src/main/java/egor/top/fnvee/core/un/UnRarService.java
+++ src/main/java/egor/top/fnvee/core/un/UnRarService.java
@@ -1,5 +1,6 @@
 package egor.top.fnvee.core.un;
 
+import com.aspose.zip.RarArchive;
 import com.github.junrar.extract.ExtractArchive;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
@@ -21,15 +22,26 @@ public class UnRarService extends UnService {
         }
 
         try {
-            log.trace("[unRar] +++ unRar +++");
+            log.trace("[unRar: junrar] +++ unRar +++");
             ExtractArchive extractArchive = new ExtractArchive();
             extractArchive.extractArchive(newMod.toFile(), newFolder.toFile());
-            log.trace("[unRar] --- unRar ---");
+            log.trace("[unRar: junrar] --- unRar ---");
             return true;
         } catch (Exception e) {
-            log.error("[unRar] cannot unRar", e);
+            log.error("[unRar: junrar] cannot unRar", e);
+            return extractAndCopyToFolderViaAspose(newMod, newFolder);
         }
+    }
 
+    private boolean extractAndCopyToFolderViaAspose(Path newMod, Path newFolder) {
+        log.trace("[unRar: aspose] +++ unRar +++");
+        try (RarArchive archive = new RarArchive(newMod.toString())) {
+            archive.extractToDirectory(newFolder.toString());
+            log.trace("[unRar: aspose] --- unRar ---");
+            return true;
+        } catch (Exception e) {
+            log.error("[unRar: aspose] cannot unRar", e);
+        }
         return false;
     }
 
